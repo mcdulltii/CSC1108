ef543a32a4563a1dbfb9085e0fb0a136bc3c8c07
mcdulltii <mcdulltii@gmail.com>
2023-03-01 09:34:24 -0500

    Modified frontend to visualize all bus routes Added bus routes in Flask GET api route Added custom logger

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask_cors import CORS
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
 import pickle
 import argparse
 import os
+
+import logging
+from custom_logging import CustomFormatter
 
 # Flask initializations
 template_dir = os.path.abspath('static/')
@@ -22,10 +25,34 @@ def main():
     return render_template('index.html', api_key=os.getenv('API_KEY'))
 
 
+@app.route('/route/<bus_number>/<int:direction>')
+def get_routes(bus_number, direction):
+    key = f'{bus_number}_{direction}'
+    if routes is None or key not in routes.keys():
+        return invalid_page('Invalid bus route')
+    return jsonify({key: [{'lat': i[1],'lng': i[0]} for i in routes[key]]})
+
+
+@app.errorhandler(404)
+def invalid_page(e):
+    return jsonify({'errorCode': 404, 'message': str(e)})
+
+
 if __name__ == '__main__':
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    ch.setFormatter(CustomFormatter())
+    logger.addHandler(ch)
+
     if args.file:
-        with open(args.file, 'r') as f:
+        with open(args.file, 'rb') as f:
             routes = pickle.load(f)
+        logger.info(f'{routes.keys()=}')
+    else:
+        logger.warning('Load bus routes file for routes visualization feature')
+        routes = None
     host = args.host if args.host else '127.0.0.1'
     port = int(args.port) if args.port else 80
     app.run(host=host, port=port)
--- static/map.js
+++ static/map.js
@@ -1,24 +1,80 @@
+// Google map
 var map;
+// Marker overlay
 var marker;
 
 function initialize() {
+  // Google map options
   var mapOptions = {
-    center: new google.maps.LatLng(-33.30578381949298, 26.523442268371582),
-    zoom: 15
+    // Hardcoded center
+    center: {lat: 1.5573879239318515, lng: 103.70745681228475},
+    // Zoom out to view all routes
+    zoom: 12.55
   };
+  // Initialize map on html canvas
   map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+  // Add mouseclick event listener to overlay marker
   google.maps.event.addListener(map, 'click', function(event){
     placeMarker(event.latLng);
+  });
+
+  // Overlay bus routes
+  overlayBusRoute('P101', 1, "#FF0000");
+  overlayBusRoute('P102', 1, "#00FF00");
+  overlayBusRoute('P102', 2, "#0000FF");
+  overlayBusRoute('P106', 1, "#0000FF");
+  overlayBusRoute('P202', 1, "#0FF000");
+  overlayBusRoute('P211', 1, "#000FF0");
+  overlayBusRoute('P211', 1, "#000FF0");
+  overlayBusRoute('P411', 1, "#F0000F");
+  overlayBusRoute('P411', 2, "#F0000F");
+  overlayBusRoute('P403', 1, "#000000");
+}
+
+function overlayBusRoute(busNumber, direction, color) {
+  // Retrieve bus routes via GET request
+  getBusRoute(busNumber, direction).then(function(busRoute) {
+    // Build bus route polyline array
+    const busPolyline = new google.maps.Polyline({
+      path: busRoute,
+      geodesic: true,
+      strokeColor: color,
+      strokeOpacity: 1.0,
+      strokeWeight: 3,
+    });
+    // Apply polyline overlay on google map
+    busPolyline.setMap(map);
   });
 }
 
 function placeMarker(location) {
   if (marker) {
+    // Overlay marker at clicked position
     marker.setPosition(location);
   } else {
+    // Initialize new marker
     marker = new google.maps.Marker({
       position: location,
       map: map
     });
   }
+}
+
+function getBusRoute(busNumber, direction) {
+  // Return bus route after GET request succeeds
+  return new Promise(function(resolve, reject) {
+    // Bus number and direction as key
+    const key = busNumber + '_' + direction;
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        var res = JSON.parse(this.responseText);
+        // Return bus route from GET request
+        resolve(res[key]);
+      }
+    };
+    // Flask api route
+    xhttp.open("GET", "/route/" + busNumber + "/" + direction, true);
+    xhttp.send();
+  });
 }
