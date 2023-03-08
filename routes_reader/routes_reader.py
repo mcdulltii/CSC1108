860df50a2a3a9954749355f6cb9e4d8923a30566
Aaron <mcdulltii@gmail.com>
2023-03-08 04:44:53 +0000

    Changed structure

--- routes_reader/routes_reader.py
+++ routes_reader/routes_reader.py
@@ -1,22 +1,21 @@
+from typing import Dict
 import pandas as pd
 import os
 
 
 class RoutesReader:
-    def __init__(self, filepath=None):
+    def __init__(self):
+        self.dirpath = os.path.dirname(os.path.realpath(__file__))
+
+    def read_excel(self, filepath: str = None) -> Dict[str, Dict]:
         # Check if excel filepath is specified
         if filepath is None:
-            # Import excel file from routesRecords.xlsx
-            dirpath = os.path.dirname(os.path.realpath(__file__))
-            self.filepath = os.path.join(dirpath, 'routeRecords.xlsx')
-        else:
-            self.filepath = filepath
-        assert self.filepath is not None
+            # Import excel file from routeRecords.xlsx
+            filepath = os.path.join(self.dirpath, 'routeRecords.xlsx')
 
-    def read_excel(self):
         # dictionary with values as list
         # use sheet_Name = [0,'Bus']
-        df = pd.read_excel(self.filepath, sheet_name='Bus')
+        df = pd.read_excel(filepath, sheet_name='Bus')
         output = df.set_index('Service Route').T.to_dict('dict')
         return output
 
