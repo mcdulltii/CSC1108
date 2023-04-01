49d3217ad61a2a0ba2955f98abe487a395c32421
ProgrammingTohster <2201170@sit.singaporetech.edu.sg>
2023-04-01 09:41:36 +0800

    adding Both astar and dijkstras in the routes

--- routing/AStarAlgo.py
+++ routing/AStarAlgo.py
@@ -1,7 +1,6 @@
 from queue import PriorityQueue
 from math import sin, cos, sqrt, atan2, radians
 
-from routing import data_algo
 
 
 class AStar:
@@ -12,10 +11,11 @@ class AStar:
         self.graph = graph
 
     def calculate_route(self, start, end):
+        self.forBusTransferFunction = None
         route = {"NodesVisited": [], "Transfers": [], "Distance": 0, "Path": []}
 
         if start not in self.graph or end not in self.graph:
-            print("Start or end node not in graph")
+            # print("Start or end node not in graph")
             return route
 
         # Priority queue with f(n) = g(n) + h(n)
@@ -30,9 +30,9 @@ class AStar:
         q.put((g[start] + h[start], start))
 
         while not q.empty():
-            print("q", q.queue)
+            # print("q", q.queue)
             node = q.get()[1]
-            print("Visiting node", node)
+            # print("Visiting node", node)
             route["NodesVisited"].append(node)
 
             if node == end:
@@ -42,21 +42,22 @@ class AStar:
 
             for i in range(3, len(self.graph[node])):
                 neighbor = self.graph[node][i]
-                print("Exploring neighbor", neighbor)
+                # print("Exploring neighbor", neighbor)
                 neighborName = neighbor["Name"]
                 if neighborName not in g:
                     g[neighborName] = float("inf")
-                weight = neighbor["Distance Away"]
+                weight = neighbor["Time Taken"]
+                print(weight)
                 transfer = self.check(neighborName)
                 transferTime = 0
                 if transfer:
                     transferTime = 15
                 if g[node] + weight + transferTime < g[neighborName]:
                     g[neighborName] = g[node] + weight + transferTime
-                    h[neighborName] = neighbor["Distance Away"]
+                    h[neighborName] = self.heuristic(neighborName, end)
                     prev[neighborName] = node
                     q.put((g[neighborName] + h[neighborName], neighborName))
-        print("prev", prev)
+        # print("prev", prev)
         if route["Distance"] == 0:
             route["Distance"] = None
         path = [end]
@@ -67,8 +68,7 @@ class AStar:
         path.reverse()
 
         route["Path"] = path
-
-        return route
+        return self.parsingToData(route, g)
 
     def heuristic(self, node, end):
         """
@@ -81,7 +81,7 @@ class AStar:
         lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
         dlon = lon2 - lon1
         dlat = lat2 - lat1
-        a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+        a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
         c = 2 * atan2(sqrt(a), sqrt(1 - a))
         distance = R * c
         return distance
@@ -93,7 +93,7 @@ class AStar:
         transfers = []
         current_node = end
         if current_node not in prev:
-            print("No route found")
+            # print("No route found")
             return transfers
         while current_node != start:
             transfer_from = prev[current_node]
@@ -105,12 +105,13 @@ class AStar:
                     t["Transfer To"] = transfer_to
                     break
             else:
-                transfers.append({"Bus": self.graph[transfer_from][0], "Transfer From": transfer_from, "Transfer To": transfer_to})
+                transfers.append(
+                    {"Bus": self.graph[transfer_from][0], "Transfer From": transfer_from, "Transfer To": transfer_to})
         transfers.reverse()
         return transfers
 
     def check(self, currentBusStop):
-        if self.forBusTransferFunction == None:
+        if self.forBusTransferFunction is None:
             self.forBusTransferFunction = set(self.graph[currentBusStop][0]["Buses Supported"])
             return False
         else:
@@ -118,22 +119,51 @@ class AStar:
                 self.forBusTransferFunction = set(self.graph[currentBusStop][0]["Buses Supported"])
                 return True
             else:
-                self.forBusTransferFunction = set(self.graph[currentBusStop][0]["Buses Supported"])
+                self.forBusTransferFunction = set(self.graph[currentBusStop][0]["Buses Supported"]) & self.forBusTransferFunction
                 return False
+
+    def parsingToData(self, route, g):
+        self.forBusTransferFunction = None
+        toReturn = {}
+        toReturn["Pathing"] = route["Path"]
+        toReturn["Buses To Return"] = []
+        toReturn["Transfers"] = []
+        timeToMinus = None
+        busToAppend = None
+        # Transfers, Distance, Path
+        prevNode = None
+        for node in route["Transfers"]:
+            prevNode = node if prevNode is None else prevNode
+            if busToAppend is None:
+                busToAppend = set(node["Bus"]["Buses Supported"])
+            else:
+                if busToAppend.isdisjoint(set(node["Bus"]["Buses Supported"])):
+                    toReturn["Buses To Return"].append(list(busToAppend))
+                    busToAppend = set(node["Bus"]["Buses Supported"])
+                    toReturn["Transfers"].append({
+                        "Transfer Stop": prevNode["Transfer From"],
+                        "Type": "On-Site",
+                        "Time Taken": g[prevNode["Transfer From"]] if timeToMinus is None else g[prevNode["Transfer From"]] - timeToMinus
+                    })
+                    timeToMinus = g[node["Transfer From"]]
+                else:
+                    busToAppend = busToAppend & set(node["Bus"]["Buses Supported"])
+            prevNode = node
+        toReturn["Buses To Return"].append(list(busToAppend))
+        toReturn["Distance"] = route["Distance"]
+        print(toReturn)
+        return toReturn
+        # for node in route["Path"]:
 
 
 # Testing
-dataobj = data_algo.RoutingAlgo()
-graph = dataobj.graphedData
-print(graph)
-aSTar = AStar(graph)
-route = aSTar.calculate_route("Majlis Bandaraya Johor Bahru", "AEON Tebrau City")
 
-if route["Distance"] is None:
-    print("No route found!")
-else:
-    print(f"Distance: {route['Distance']:.2f} km")
-    print("Nodes visited: ", route["NodesVisited"])
-    print("Transfers: ")
-    for t in route["Transfers"]:
-        print(f"Take bus {t['Bus']} from {t['Transfer From']} to {t['Transfer To']}")
+#
+# if route["Distance"] is None:
+#     print("No route found!")
+# else:
+#     print(f"Distance: {route['Distance']:.2f} km")
+#     print("Nodes visited: ", route["NodesVisited"])
+#     print("Transfers: ")
+#     for t in route["Transfers"]:
+#         print(f"Take bus {t['Bus']} from {t['Transfer From']} to {t['Transfer To']}")
